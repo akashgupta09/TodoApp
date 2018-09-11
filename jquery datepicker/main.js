630b8acdf5381f0963645129456a546f9dd18845
akashgupta09 <akashgupta2897@gmail.com>
2018-09-11 15:00:05 +0530

    How to use jquery datepicker

---
+++ jquery datepicker/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#datepicker").datepicker();
+    $("#datepicker1").datepicker();
+    $("#datepicker2").datepicker();
+    $("#datepicker").datepicker("show");
+})
