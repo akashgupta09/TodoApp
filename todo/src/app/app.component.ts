d4d87b430874b17fd222d0b1360f70710368035d
akashgupta09 <akashgupta2897@gmail.com>
2018-09-14 16:32:07 +0530

    Complete Todo App

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -8,42 +8,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+public newTodo;
+public trackIndex;
+
   title = 'todo App';
-  names=["akash","mumbai",1]
+  names = ["akash", "mumbai", 1]
   // text={
   //   name:"akash",
   //   city:"mumbai",
 
   // }
-   todos = [
+  todos = [
     {
       label: 'Call Akash',
-     done: true,
-      
+      done: true,
+
     },
     {
       label: 'Pay Bill',
-    done: false,
-   
+      done: false,
+
     },
     {
       label: 'Go to Gym',
       done: true,
-    
-   }
- ]; 
 
- addTodo(todo) {
- var newTodo = {
-   label: todo,
-   done:true
-};
-this.todos.push(newTodo);
- }
+    }
+  ];
 
- deleteTodo(todo){  
-   let index=this.todos.indexOf(todo); 
-this.todos.splice(index,1);
+  addTodo(todo) {
+      var newTodo = {
+        label: todo,
+        done: true
+      };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+editTodo(todo,idx){
+  console.log(idx);
+  console.log(this.todos[idx]);
+  console.log (this.todos[idx].label);
+  this.newTodo = this.todos[idx].label;
+  
+  this.trackIndex=idx;
+console.log(this.trackIndex);
+
+
+  
+
+
+
+  //  let result = prompt("update your todo app", label);
+  //   if (result !== null && result !== ""){
+  //     this.todos[todo].label = result;
+ 
+   
+  
+    }
+updateTodo(newValue){
+  console.log(this.trackIndex);
+ this.todos[this.trackIndex].label=newValue;
+ 
 }
-}
+
+  }
+
+
+
+ 
+
+
+
+
+
